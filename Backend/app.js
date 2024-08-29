5eaccb5e8cbaaabcb2b6ee98db889e55edb74420
Dulneth210229 <dulneth.sa@gmail.com>
2024-08-29 13:36:11 +0530

    change the port number

--- Backend/app.js
+++ Backend/app.js
@@ -12,6 +12,6 @@ mongoose
   .connect("mongodb+srv://mern:mern@cluster0.icy1i.mongodb.net/")
   .then(() => console.log("MongoDB connected"))
   .then(() => {
-    app.listen(5000, () => console.log("Server is running on port 5000"));
+    app.listen(5001, () => console.log("Server is running on port 5000"));
   })
   .catch((err) => console.log(err));
