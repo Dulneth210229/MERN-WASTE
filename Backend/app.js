2b235b96191952f98cf3ebcecbbb64b28dc18942
Dulneth210229 <dulneth.sa@gmail.com>
2024-08-29 18:38:29 +0530

    Create inventory control, model and route files

--- Backend/app.js
+++ Backend/app.js
@@ -1,17 +1,16 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const router = require("./Routes/InventoryRoute");
 
 const app = express();
 
 //middleware
-app.use("/", (req, res, next) => {
-  res.send("It is working");
-});
+app.use("/inventory", router);
 
 mongoose
   .connect("mongodb+srv://mern:mern@cluster0.icy1i.mongodb.net/")
   .then(() => console.log("MongoDB connected"))
   .then(() => {
-    app.listen(5001, () => console.log("Server is running on port 5000"));
+    app.listen(5001, () => console.log("Server is running on port 5001"));
   })
   .catch((err) => console.log(err));
