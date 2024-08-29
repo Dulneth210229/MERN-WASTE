5901e8b4bd828c8753c0f435b9095abd31583e02
Dulneth210229 <dulneth.sa@gmail.com>
2024-08-29 21:48:37 +0530

    change route name router to inventoryrouter

--- Backend/Controllers/RequestController.js
+++ Backend/Controllers/RequestController.js
@@ -1,65 +1,63 @@
-const Request = require('../Model/RequestModel');
+const Request = require("../Model/RequestModel");
 
 //data display
 const getAllRequests = async (req, res, next) => {
+  let requests;
 
-    let requests;
+  //Get all Requests
+  try {
+    requests = await Request.find();
+  } catch (err) {
+    console.log(err);
+  }
 
-    //Get all Requests
-    try {
-        requests = await Request.find();
-    } catch (err) {
-        console.log(err);
-    }
+  //not found
+  if (!requests) {
+    return res.status(404).json({ message: "Request not found" });
+  }
 
-    //not found
-    if (!requests) {
-        return res.status(404).json({ message: "Request not found" });
-    }
-
-    //Display all users
-    return res.status(200).json({ requests: requests });
+  //Display all users
+  return res.status(200).json({ requests: requests });
 };
 
 //data insert
 const addRequests = async (req, res, next) => {
+  const { service, name, address, phoneNumber, date, time } = req.body;
 
-    const { service, name, address, phoneNumber, date, time } = req.body;
+  let request;
 
-    let request;
+  try {
+    request = new Request({ service, name, address, phoneNumber, date, time });
+    await request.save();
+  } catch (err) {
+    console.log(err);
+  }
 
-    try {
-        request = new Request({service, name, address, phoneNumber, date, time});
-        await request.save();
-    } catch (err) {
-        console.log(err);
-    }
-
-    //not insert users
-    if (!request) {
-        return res.status(404).json({ message: "Unable to add request" });
-    }
-    return res.status(200).json({ request });
+  //not insert users
+  if (!request) {
+    return res.status(404).json({ message: "Unable to add request" });
+  }
+  return res.status(200).json({ request });
 };
 
 //Get by ID
 const getRequestById = async (req, res, next) => {
-    const id = req.params.id;
+  const id = req.params.id;
 
-    let request;
+  let request;
 
-    try {
-        request = await Request.findById(id);
-    } catch (err) {
-        console.log(err);
-    }
+  try {
+    request = await Request.findById(id);
+  } catch (err) {
+    console.log(err);
+  }
 
-    //not available users
-    if (!request) {
-        return res.status(404).json({ message: "Request not found" });
-    }
-    return res.status(200).json({ request });
-}
+  //not available users
+  if (!request) {
+    return res.status(404).json({ message: "Request not found" });
+  }
+  return res.status(200).json({ request });
+};
 
 exports.getAllRequests = getAllRequests;
 exports.addRequests = addRequests;
