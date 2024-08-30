1d3b9d1258c4475eb4178d0882e8e0b54ae2fd71
Dulneth210229 <dulneth.sa@gmail.com>
2024-08-30 18:58:57 +0530

    Create inventory stock CRUD operations

--- Backend/app.js
+++ Backend/app.js
@@ -2,10 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const inventoryrouter = require("./Routes/InventoryRoute");
 const requestRouter = require("./Routes/RequestRoute");
-const paymentplanrouter=require("./Routes/PaymentPlanRoute");
-
-
-
+const paymentplanrouter = require("./Routes/PaymentPlanRoute");
 const feedbackRouter = require("./Routes/FeedbackRoutes");
 
 const app = express();
@@ -14,10 +11,8 @@ const app = express();
 app.use(express.json());
 app.use("/request", requestRouter);
 app.use("/inventory", inventoryrouter);
-app.use("/paymentplan",paymentplanrouter);
+app.use("/paymentplan", paymentplanrouter);
 app.use("/feedback", feedbackRouter);
-
-
 
 mongoose
   .connect("mongodb+srv://mern:mern@cluster0.icy1i.mongodb.net/")
