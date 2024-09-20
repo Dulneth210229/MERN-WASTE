d25d0787c1b2247ac574104e213c5a3f63888893
Dulneth210229 <dulneth.sa@gmail.com>
2024-09-20 19:36:20 +0530

    nn

--- frontend/src/Components/Inventory/Report/Report.js
+++ frontend/src/Components/Inventory/Report/Report.js
@@ -64,9 +64,9 @@ function Report() {
         </div>
 
         {/* Hidden Printable Order */}
-        <div style={{ display: "none" }}>
+        {/* <div style={{ display: "none" }}>
           <PrintableOrder ref={componentsRef} />
-        </div>
+        </div> */}
         <div className="flex flex-row gap-20 mx-auto w-3/3 mt-5 mr-10 ml-10 ">
           <div className="relative w-1/3 h-52 bg-slate-100 rounded-lg m-3 shadow-2xl">
             <div className=" w-auto h-12 bg-green-200 rounded-lg  ">
