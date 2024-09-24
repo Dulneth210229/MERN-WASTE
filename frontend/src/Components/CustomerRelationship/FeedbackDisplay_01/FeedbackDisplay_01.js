fd529356db7728e39330c20019890f9cb163621b
Tharuka Dilshan Oberathna <oberathna2020@gmail.com>
2024-09-24 19:52:34 +0530

    Make some changes

--- frontend/src/Components/CustomerRelationship/FeedbackDisplay_01/FeedbackDisplay_01.js
+++ frontend/src/Components/CustomerRelationship/FeedbackDisplay_01/FeedbackDisplay_01.js
@@ -39,26 +39,7 @@ function FeedbackDisplay_01() {
         </div>
       </div>
 
-        {/* <table className="border-2 mx-auto">
-          <thead>
-            <tr className="bg-green-200">
-              <th className="border-2 p-2 w-40 border-green-500">Full Name</th>
-              <th className="border-2 p-2 w-48 border-green-500">Email</th>
-              <th className="border-2 p-2 w-48 border-green-500">Address</th>
-              <th className="border-2 p-2 w-24 border-green-500">Phone</th>
-              <th className="border-2 p-2 w-20 border-green-500">Comment</th>
-              <th className="border-2 p-2 w-20 border-green-500">Rating</th>
-              <th className="border-2 p-2 w-20 border-green-500">Action</th>
-            </tr>
-          </thead>
-          <tbody>
-            {feedback &&
-              feedback.map((feedback, i) => (
-                <Feedback key={i} feedback={feedback} />
-              ))}
-          </tbody>
-        </table> */}
-
+        
       </div>
 
       <button onClick={handlePrint} className="mt-4 px-4 py-2 bg-green-600 text-white font-semibold rounded-lg shadow-md hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-400 focus:ring-opacity-75" >Download feedback Report</button>
