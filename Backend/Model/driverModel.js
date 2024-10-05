a4243a15b9a7960ce9b606883bb01bac681fd0a3
Dulneth210229 <dulneth.sa@gmail.com>
2024-10-05 20:34:20 +0530

    Changes on inventory

--- Backend/Model/driverModel.js
+++ Backend/Model/driverModel.js
@@ -1,32 +1,31 @@
 const mongoose = require("mongoose");
-const { Schema } = mongoose;  
+const { Schema } = mongoose;
 
 const driverSchema = new Schema({
-     name:{
-        type:String,
-        required:true,//validate
-     },
+  name: {
+    type: String,
+    required: true, //validate
+  },
 
-     email:{
-        type:String,
-        required:true,//validate
-     },
+  email: {
+    type: String,
+    required: true, //validate
+  },
 
-     NID:{
-        type:String,
-        required:true,//validate
-     },
+  NID: {
+    type: String,
+    required: true, //validate
+  },
 
-     Dlicense:{
-        type:String,
-        required:true,//validate
-     },
+  Dlicense: {
+    type: String,
+    required: true, //validate
+  },
 
-     password: {
-      type: String,
-      required: true,  
-    },
-
+  password: {
+    type: String,
+    required: true,
+  },
 });
 
 module.exports = mongoose.model("Driver", driverSchema);
