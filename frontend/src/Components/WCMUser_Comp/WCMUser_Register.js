b913597b96eb419388783bd56f3239e72a43976d
Dulneth210229 <dulneth.sa@gmail.com>
2024-10-06 23:59:21 +0530

    user register form

--- frontend/src/Components/WCMUser_Comp/WCMUser_Register.js
+++ frontend/src/Components/WCMUser_Comp/WCMUser_Register.js
@@ -87,129 +87,133 @@ function WCMUser_Register() {
   };
 
   return (
-    <div>
+    <div className="min-h-screen flex flex-col">
       <UserHomeHeader />
-      <div className="max-w-lg mx-auto bg-green-200 p-8 shadow-md rounded-lg mt-10">
-        <h1 className="text-2xl font-semibold text-gray-700 mb-6 text-center">
-          User Registration
-        </h1>
-        <form onSubmit={handleSubmit} className="">
-          <div className="mb-4">
-            <label
-              htmlFor="name"
-              className="block text-sm font-medium text-gray-700 mb-1"
-            >
-              Name
-            </label>
-            <input
-              type="text"
-              name="name"
-              id="name"
-              required
-              value={user.name}
-              onChange={handleInputChange}
-              className={`w-full px-3 py-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 ${
-                errors.name
-                  ? "border-red-500 focus:ring-red-500"
-                  : "focus:ring-blue-500"
-              }`}
-            />
-            {errors.name && (
-              <p className="text-red-500 text-sm">{errors.name}</p>
-            )}
-          </div>
+      <div className="bg-gray-100 p-1">
+        <div className="max-w-lg mx-auto bg-white p-8 shadow-lg rounded-lg mt-2 ">
+          <h1 className="text-2xl font-semibold text-slate-800 mb-6 text-center">
+            User Registration
+          </h1>
+          <form onSubmit={handleSubmit} className="">
+            <div className="mb-4">
+              <label
+                htmlFor="name"
+                className="block text-sm font-medium text-gray-700 mb-1"
+              >
+                Name
+              </label>
+              <input
+                type="text"
+                name="name"
+                id="name"
+                required
+                value={user.name}
+                onChange={handleInputChange}
+                className={`w-full px-3 py-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 ${
+                  errors.name
+                    ? "border-red-500 focus:ring-red-500"
+                    : "focus:ring-blue-500"
+                }`}
+              />
+              {errors.name && (
+                <p className="text-red-500 text-sm">{errors.name}</p>
+              )}
+            </div>
 
-          <div className="mb-4">
-            <label
-              htmlFor="NID"
-              className="block text-sm font-medium text-gray-700 mb-1"
-            >
-              NID
-            </label>
-            <input
-              type="text"
-              name="NID"
-              id="NID"
-              required
-              value={user.NID}
-              onChange={handleInputChange}
-              className={`w-full px-3 py-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 ${
-                errors.NID
-                  ? "border-red-500 focus:ring-red-500"
-                  : "focus:ring-blue-500"
-              }`}
-            />
-            {errors.NID && <p className="text-red-500 text-sm">{errors.NID}</p>}
-          </div>
+            <div className="mb-4">
+              <label
+                htmlFor="NID"
+                className="block text-sm font-medium text-gray-700 mb-1"
+              >
+                NID
+              </label>
+              <input
+                type="text"
+                name="NID"
+                id="NID"
+                required
+                value={user.NID}
+                onChange={handleInputChange}
+                className={`w-full px-3 py-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 ${
+                  errors.NID
+                    ? "border-red-500 focus:ring-red-500"
+                    : "focus:ring-blue-500"
+                }`}
+              />
+              {errors.NID && (
+                <p className="text-red-500 text-sm">{errors.NID}</p>
+              )}
+            </div>
 
-          <div className="mb-4">
-            <label
-              htmlFor="address"
-              className="block text-sm font-medium text-gray-700 mb-1"
-            >
-              Address
-            </label>
-            <input
-              type="text"
-              name="address"
-              id="address"
-              required
-              value={user.address}
-              onChange={handleInputChange}
-              className="w-full px-3 py-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
-            />
-          </div>
+            <div className="mb-4">
+              <label
+                htmlFor="address"
+                className="block text-sm font-medium text-gray-700 mb-1"
+              >
+                Address
+              </label>
+              <input
+                type="text"
+                name="address"
+                id="address"
+                required
+                value={user.address}
+                onChange={handleInputChange}
+                className="w-full px-3 py-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
+              />
+            </div>
 
-          <div className="mb-4">
-            <label
-              htmlFor="email"
-              className="block text-sm font-medium text-gray-700 mb-1"
-            >
-              Email
-            </label>
-            <input
-              type="email"
-              name="email"
-              id="email"
-              required
-              value={user.email}
-              onChange={handleInputChange}
-              className={`w-full px-3 py-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 ${
-                errors.email
-                  ? "border-red-500 focus:ring-red-500"
-                  : "focus:ring-blue-500"
-              }`}
-            />
-            {errors.email && (
-              <p className="text-red-500 text-sm">{errors.email}</p>
-            )}
-          </div>
+            <div className="mb-4">
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium text-gray-700 mb-1"
+              >
+                Email
+              </label>
+              <input
+                type="email"
+                name="email"
+                id="email"
+                required
+                value={user.email}
+                onChange={handleInputChange}
+                className={`w-full px-3 py-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 ${
+                  errors.email
+                    ? "border-red-500 focus:ring-red-500"
+                    : "focus:ring-blue-500"
+                }`}
+              />
+              {errors.email && (
+                <p className="text-red-500 text-sm">{errors.email}</p>
+              )}
+            </div>
 
-          <div className="mb-6">
-            <label
-              htmlFor="password"
-              className="block text-sm font-medium text-gray-700 mb-1"
-            >
-              Password
-            </label>
-            <input
-              type="password"
-              name="password"
-              id="password"
-              required
-              value={user.password}
-              onChange={handleInputChange}
-              className="w-full px-3 py-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
-            />
-          </div>
+            <div className="mb-6">
+              <label
+                htmlFor="password"
+                className="block text-sm font-medium text-gray-700 mb-1"
+              >
+                Password
+              </label>
+              <input
+                type="password"
+                name="password"
+                id="password"
+                required
+                value={user.password}
+                onChange={handleInputChange}
+                className="w-full px-3 py-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
+              />
+            </div>
 
-          <button
-            type="submit"
-            className="w-full bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition duration-300"
-          >
-            Register
-          </button>
-        </form>
+            <button
+              type="submit"
+              className="w-full bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition duration-300"
+            >
+              Register
+            </button>
+          </form>
+        </div>
       </div>
     </div>
   );
